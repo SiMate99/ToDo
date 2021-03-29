dd6624bcbf65286e95b80050d975e19894c74fd5
SiMate99 <sibakmate@gmail.com>
2021-03-29 12:03:36 +0200

    alap

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sibak Máté</title>
+    </head>
+    <body>
+        <main>
+            <header><h1>Teendők</h1></header>
+            <section>
+                
+            </section>
+        </main>
+    </body>
+</html>
